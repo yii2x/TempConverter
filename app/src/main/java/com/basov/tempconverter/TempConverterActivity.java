f8f2eb15caac4ff17daf87e5513b5e813c409297
Yuriy Basov <basowy@gmail.com>
2017-06-14 19:20:30 -0700

    Initial commit

---
+++ app/src/main/java/com/basov/tempconverter/TempConverterActivity.java
@@ -0,0 +1,13 @@
+package com.basov.tempconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TempConverterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp_converter);
+    }
+}
